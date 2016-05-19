23c93574c2c276f725734c369706cb01aadd2c19
Trey Woodlief <adwiii7@gmail.com>
2016-05-18 21:42:46 -0400

    Added very basic cookie functionality

--- NC FTC Inspection/Resources/fieldUpdate.js
+++ NC FTC Inspection/Resources/fieldUpdate.js
@@ -2,4 +2,5 @@ function update(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("POST", "field?team="+event.target.name+"&value="+event.target.value, true);
 	xhttp.send();
+	document.cookie = "username=FTC_VERIFIED";
 }
